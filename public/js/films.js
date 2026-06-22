685a87f61b2320b9a00d0646a4bd788d48c4baff
Bayuptbga <114854983+Bayuptbga@users.noreply.github.com>
2026-06-22 23:27:38 +0800

    Update films.js

--- public/js/films.js
+++ public/js/films.js
@@ -20,10 +20,10 @@ function film(title, desc, embed, color1, color2) {
 const FILMS = [
 
   film('Japannese',    'Drama Japan', 'https://www.xvideos.com/embedframe/okkbeedc45d', '#1c2b52', '#2E6FF2'),
-  film('Japannese Fm', 'Japan',       'https://www.xvideos.com/video.hoduidkedcf/b7', '#3d1f52', '#A855F7'),
-  film('Japannese Moview', 'Japan',   'https://www.xvideos.com/video.ppbveh8097/av2631.part2', '#3d1f52', '#A855F7'),
-  film('Japannese Family', 'Japan',   'https://www.xvideos.com/video.uellueb651a/kotone_kuroki_gets_an_asian_creampie_massage_leading_to_an_explosive_xxx_experience.', '#3d1f52', '#A855F7'),
-  film('Japannese GB', 'Japan',       'https://www.xvideos.com/video.uukufdfe872/japanese_girl_aiko_nagai_got_gangbanged_uncensored', '#3d1f52', '#A855F7'),
+  film('Japannese Fm', 'Japan',       'https://www.xvideos.com/embedframe/uellueb651a', '#3d1f52', '#A855F7'),
+  film('Japannese Moview', 'Japan',   'https://www.xvideos.com/embedframe/udptlbbc307', '#3d1f52', '#A855F7'),
+  film('Japannese Family', 'Japan',   'https://www.xvideos.com/embedframe/kvceumk9d12.', '#3d1f52', '#A855F7'),
+  film('Japannese GB', 'Japan',       'https://www.xvideos.com/embedframe/oiopumb63e9', '#3d1f52', '#A855F7'),
   // Tambah film baru di sini ↓
   // film('Judul Film',  'Genre', 'https://...', '#warna1', '#warna2'),
 
